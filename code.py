5e6c5747b6df077ea1ce066e7668683899180bc6
monicaaaaaaaaa <42147361+monicaaaaaaaaa@users.noreply.github.com>
2018-08-09 11:43:04 -0500

    Update code.py
    turns on the LEDs one by one using different colors in python.

--- code.py
+++ code.py
@@ -4,15 +4,30 @@ import time
 white = (30, 30, 30)
 pink = (40, 0, 30)
 blue = (0, 0, 50)
-turquoise = (64, 220, 100)
+
 
 while True:
     for i in range (0, 10):
         cpx.pixels[0] = white
-        time.sleep (.5)
+        time.sleep(.5)        
         cpx.pixels[1] = pink
-        time.sleep (.5)
-        cpx.pixels[2] = turquoise
         time.sleep(.5)
-        cpx.pixels[3] = blue
+        cpx.pixels[2] = blue
+        time.sleep(.5)
+        cpx.pixels[3] = white
+        time.sleep(.5)
+        cpx.pixels[4] = pink
+        time.sleep(.5)
+        cpx.pixels[5] = blue
+        time.sleep(.5)
+        cpx.pixels[6] = white
+        time.sleep(.5)
+        cpx.pixels[7] = pink
+        time.sleep(.5)
+        cpx.pixels[8] = blue
+        time.sleep(.5)
+        cpx.pixels[9] = white
+        time.sleep(.5)
+        cpx.pixels.fill ((0, 0, 0))
+
         
